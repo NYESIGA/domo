8da77f91755c95072a3876d0795472ff41ba618c
NYESIGA <nyesigah65@gmail.com>
2023-02-27 11:03:41 +0800

    addea getters and setters

--- src/com/josihper/studentControllers/studentDetails.java
+++ src/com/josihper/studentControllers/studentDetails.java
@@ -1,4 +1,48 @@
 package com.josihper.studentControllers;
 
 public class studentDetails {
+    private int id;
+    private  String name;
+    private  String email;
+
+    public studentDetails(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    private  String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
